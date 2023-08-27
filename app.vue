a6072b66013965edeffa1e1b433050ecd03aac03
Dorian <dorian@resalocal.com>
2023-08-27 17:51:01 +0200

    add pusher

--- app.vue
+++ app.vue
@@ -7,7 +7,26 @@ import {InputSystem} from "~/game/systems/input-system";
 import {MoveSystem} from "~/game/systems/move-system";
 import {RenderSystem} from "~/game/systems/render-system";
 
+useHead({
+  script: [
+    {src: "https://js.pusher.com/8.2.0/pusher.min.js"},
+    {src: "https://cdn.jsdelivr.net/npm/vue/dist/vue.js"},
+  ],
+});
+
 onMounted(() => {
+  // Enable pusher logging - don't include this in production
+  // Pusher.logToConsole = true;
+
+  const pusher = new Pusher("8e159b83fa8e7f9550d7", {
+    cluster: "eu",
+  });
+
+  const channel = pusher.subscribe("my-channel");
+  channel.bind("my-event", function (data: any) {
+    console.log(data);
+  });
+
   const game = new Game({
     systems: [new InputSystem(), new MoveSystem(), new RenderSystem()],
     resources: [{type: "client-id", id: 0}],
